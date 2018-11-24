5a508bece31d26da56c8ab69989bf7743bffdf55
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-11-24 15:42:53 -0500

    SeedsFromMasjidTucson.getters_iterator up to parsed_chapter_info_to_hash()

--- backend/app/models/seeds_from_masjid_tucson.rb
+++ backend/app/models/seeds_from_masjid_tucson.rb
@@ -13,23 +13,49 @@ class SeedsFromMasjidTucson #likely will need to change class name once data is 
     # produce a hash of all of the data, so that seeding can work independently of internet
     (1..114).to_a.map do |num|
 
+      chapter_translation_text_array = chapter_translation_parser(num)
 
-      # chapter_translation_document = Nokogiri::HTML.parse(open(ch_url_constructor(num)))
-      # chapter_footnotes_document = Nokogiri::HTML.parse(open(ch__fn_url_constructor(num)))
+      chapter_footnotes_text_array = chapter_footnotes_parser(num)
 
-      # nokogiri_documents_to_hash(chapter_translation_document, chapter_footnotes_document)
-
-
-
+      parsed_chapter_info_to_hash(chapter_translation_text_array, chapter_footnotes_text_array)
     end
 
   end
 
   private
 
-  def nokogiri_documents_to_hash(ng_obj)
-    # document.inner_html or document.inner_text appears to work
+  def parsed_chapter_info_to_hash(chapter_translation_array, chapter_footnotes_array)
+  
+  end
 
+  def chapter_translation_parser(chapter_number)
+    # chapter translation related
+    chapter_translation_document = Nokogiri::HTML.parse(open(ch_url_constructor(chapter_number)))
+    chapter_verses_html_array = chapter_translation_document.css("p a").select do |html|
+      !html.inner_text.include?("Footnote")
+    end
+
+    chapter_verses_html_array.map do |verse_html|
+      # splice out \t (horizontal tab) if needed
+      if !verse_html.inner_text.include?("\t")
+        verse_html.inner_text
+      else
+        verse_html.inner_text.split("\t").join("")
+      end
+    end
+  end
+
+  def chapter_footnotes_parser(chapter_number)
+    # chapter footnotes related
+    chapter_footnotes_document = Nokogiri::HTML.parse(open(ch_fn_url_constructor(chapter_number)))
+    chapter_footnotes_document.css("p, table").map do |html|
+      if html.name == "p"
+        html.inner_text.split(/[\r\n\t]/).join("")
+      else
+        # only footnotes for chapter 1 translation has table, so this can be very specific parsing
+        "<table>" +  html.inner_html.split(/[\r\n\s]/).join("") + "</table>"
+      end
+    end
   end
 
   def ch_url_constructor(chapter_number)
