4035cd5bae965f1a1504618d64c37d6b8982474e
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-11-24 14:01:09 -0500

    defined initial logic flow for SeedsFromMasjidTucson.getters_iterator

--- backend/app/models/seeds_from_masjid_tucson.rb
+++ backend/app/models/seeds_from_masjid_tucson.rb
@@ -2,22 +2,44 @@
 # "https://www.masjidtucson.org/quran/frames/ch#{chapter_number}.html"
 
 require "byebug"
-require "rest-client"
+require "nokogiri"
 
 class SeedsFromMasjidTucson
 
   def getters_iterator
+    # would like to transition from get requests to localized json asap
+
     # initialize all 114 get requests to respective target urls
-    (1..114).to_a.each do |num|
-      byebug
-      resp = RestClient.get(url_constructor(num))
-      
+    # produce a hash of all of the data, so that seeding can work independently of internet
+    (1..114).to_a.map do |num|
+
+
+      # translation_document = Nokogiri::HTML.parse(open(ch_url_constructor(num)))
+      # footnotes_document = Nokogiri::HTML.parse(open(ch__fn_url_constructor(num)))
+
+      # nokogiri_documents_to_hash(translation_document, footnotes_document)
+
+
+
     end
+
   end
 
   private
 
-  def url_constructor(chapter_number)
+  def nokogiri_documents_to_hash(ng_obj)
+    # document.inner_html or document.inner_text appears to work
+
+  end
+
+  def ch_url_constructor(chapter_number)
+    # url for chapter translation
     "https://www.masjidtucson.org/quran/frames/ch#{chapter_number}.html"
+  end
+
+  def ch_fn_url_constructor(chapter_number)
+    # url for chapter footnotes
+    # all chapter tranlsations have footnotes
+    "https://www.masjidtucson.org/quran/frames/ch#{chapter_number}fn.html"
   end
 end
