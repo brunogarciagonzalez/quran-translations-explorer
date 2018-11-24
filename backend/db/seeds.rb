cc95b8859aa5d3150695b57e1b4837d637f3b53d
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-11-24 14:09:26 -0500

    fixed pseudocode spelling mistake

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,11 +6,11 @@ Translation.destroy_all
 
 puts "**************"
 
-# for data from .txt translations
+# for data from .txt translations:
 # seeder_txts = SeedsFromTxts.new
 # seeder_txts.directory_iterator
 
-# for data of Rashad's translation from masjidtucson.com
+# for data of Rashad's translation from masjidtucson.org:
 seeder_masjid_tucson = SeedsFromMasjidTucson.new
 seeder_masjid_tucson.getters_iterator
 
