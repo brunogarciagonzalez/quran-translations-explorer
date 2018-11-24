39b343a5e2c6f898a2c80977dd4589094f3180a4
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-11-24 12:25:47 -0500

    beginning class SeedsFromMasjidTucson

---
+++ backend/app/models/seeds_from_masjid_tucson.rb
@@ -0,0 +1,10 @@
+# each html file to be parsed is acquired from:
+# "https://www.masjidtucson.org/quran/frames/ch#{chapter_number}.html"
+
+
+require "rest-client"
+
+class SeedsFromMasjidTucson
+
+
+end
